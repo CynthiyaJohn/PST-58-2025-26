c5f0e5c903e50a518086a182980fda9ab7b0443b
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-01-26 23:28:30 +0530

    Add files via upload

---
+++ Array+String/Reverse Array Declration.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(" Reverse Array Declaration:");
+		 
+		 int arr[]={0,1,2,3,4,5};
+		 for(int i=5;i>=0;i--){
+		     System.out.println(arr[i]);
+		 }
+	}
+}
