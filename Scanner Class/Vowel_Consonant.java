b785fe4e53f08df6292886d76571a48523167e40
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-01-27 09:17:38 +0530

    Add files via upload

---
+++ Scanner Class/Vowel_Consonant.java
@@ -0,0 +1,22 @@
+package PST;
+
+import java.util.Scanner;
+
+public class Vowel_Consonant {
+
+	public static void main(String[] args) {
+		System.out.println("Program to Read a Character and Find if It is a Vowel Or Consonant");
+        
+		System.out.println("Enter a character :");
+	      Scanner sc = new Scanner(System.in);
+	      char ch = sc.next().charAt(0);
+	      if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
+	         System.out.println("Given character is an vowel");
+	      }else{
+	         System.out.println("Given character is a consonant");
+	      }
+		
+	    sc.close();
+	}
+
+}
