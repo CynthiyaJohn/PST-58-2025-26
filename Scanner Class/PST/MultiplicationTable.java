065b96517354e752d96b987c910c88e282ea2fd1
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-02-02 23:44:00 +0530

    Add files via upload

---
+++ Scanner Class/PST/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package PST;
+
+
+import java.util.Scanner;
+
+public class MultiplicationTable
+{
+	public static void main(String[] args) {
+	    System.out.println(" THE MULTIPLICATION TABLE ");
+		Scanner sc= new Scanner(System.in);
+	    
+	    System.out.println(" Enter any Number:");
+	    int num=sc.nextInt();
+	    
+	    for(int i=0;i<=10;i++){
+	        System.out.println(num+"x"+i+"="+(num*i));
+	    }
+	}
+}
