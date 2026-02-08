31fac24853b03d810b1f4ea4040e12c582cea8c3
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-02-08 19:30:18 +0530

    Add files via upload

---
+++ Scanner Class/PST/PerfectNum.java
@@ -0,0 +1,20 @@
+package PST;
+
+import java.util.Scanner;
+
+public class PerfectNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) sum += i;
+        }
+
+        System.out.println(num + ((sum == num && num > 0) ? " is perfect." : " is not perfect."));
+        
+        sc.close();
+    }
+}
