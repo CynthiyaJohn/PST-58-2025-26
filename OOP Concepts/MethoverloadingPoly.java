573e21c323edf540b54a72390b079b845b3a39db
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-02-08 19:34:01 +0530

    Add files via upload

---
+++ OOP Concepts/MethoverloadingPoly.java
@@ -0,0 +1,27 @@
+package PST;
+
+//Method overloading_compiletime Polymorphism
+
+public class MethoverloadingPoly {
+
+  public static void main(String[] args) {
+      method();
+  }
+
+  public static void method() {
+      int a=11,b=13,c=15;
+      
+      System.out.println(method1(a,b));
+      System.out.println(method1(a,b,c));
+      
+  }
+
+  public static int method1(int a,int b) {
+    
+      return a+b;
+  }
+  public static int method1(int a,int b,int c) {
+     
+      return a+b+c;
+  }
+}
