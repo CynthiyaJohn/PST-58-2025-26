6d214b22c9a7885cebd5584ee8383158b088acb2
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-02-11 12:23:33 +0530

    Add files via upload

---
+++ Convertions/ConvChStrBin.java
@@ -0,0 +1,17 @@
+package PST;
+
+
+public class ConvChStrBin {
+
+	public static void main(String[] args) {
+		
+		char ch='A';
+		String str=String.valueOf(ch);
+		String str1=ch +"";
+		
+		System.out.println("The Conversion of Character To String: "+str);
+		System.out.println("The Conversion: "+str1);
+	
+	}
+
+}
