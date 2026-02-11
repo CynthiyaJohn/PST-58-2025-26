f62398c8fc0c743a26fd8a46d143ff812fdfb409
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-02-11 12:22:48 +0530

    Add files via upload

---
+++ Convertions/ConvIntStr.java
@@ -0,0 +1,24 @@
+package PST;
+
+import java.util.Scanner;
+
+
+public class ConvIntStr {
+
+    public static void main(String[] args) {
+
+        String s = "123";
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);      // step 3
+            int digit = ch - '0';       // step 4 (ASCII logic)
+
+            result = result * 10 + digit; // step 5
+        }
+
+        System.out.println(result);     // 123
+    }
+}
+
