07b86c6e0893ab5b04018071936aa84dd220c034
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-02-11 12:23:16 +0530

    Add files via upload

---
+++ Convertions/ConvDoubStrBin.java
@@ -0,0 +1,17 @@
+package PST;
+
+import java.util.Scanner;
+
+
+public class ConvDoubStrBin {
+
+    public static void main(String[] args) {
+
+        double num = 567.45;
+
+        String str = Double.toString(num);
+
+        System.out.println("Converted String: " + str);
+    }
+ }
+
