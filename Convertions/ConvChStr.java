036935fb4e394e802c2c86b4208a0a457f1a3a64
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2026-02-11 12:23:55 +0530

    Add files via upload

---
+++ Convertions/ConvChStr.java
@@ -0,0 +1,14 @@
+package PST;
+
+
+public class ConvChStr {
+    public static void main(String[] args) {
+        char ch = 'S';
+       
+        String str = "" + ch; 
+        
+        System.out.println("The character " + ch + " is a String: " + str);
+        
+        System.out.println("String length: " + str.length());
+    }
+}
